acb84c3e6c1ebbdf74a9da4c95ae1aa217340fb0
//AI's are better than humans <57877338+ai-are-better-than-humans@users.noreply.github.com>
2020-11-06 20:36:29 -0500

    Update ImageView.py

--- ImageView.py
+++ ImageView.py
@@ -1,16 +1,13 @@
 from matplotlib.pyplot import imshow, show
 import numpy as np
-from sys import argv
 
-pt1 = r"C:\Users\mlfre\OneDrive\Desktop\img_temp\pixels.txt"
-pt2 = r"C:\Users\mlfre\OneDrive\Desktop\img_temp\info.txt"
-#f = open(input("pix:\n"), "r")
-#f2 = open(input("info:\n"), "r")
-f = open(pt1, "r")
-f2 = open(pt2, "r")
+
+f = open(r"<Desktop Location>\Desktop\img_temp\pixels.txt", "r")
+f2 = open(r"<Desktop Location>\Desktop\img_temp\info.txt", "r")
+
 newvals = [int(x) for x in f.readline().split(',') if x != '\n']
-
 info = [int(x) for x in f2.readline().split(',') if x != '\n']
+
 width = info[0]
 height = info[1]
 bpx = info[2]
